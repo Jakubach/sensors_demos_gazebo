b458997890aa8b18b79ec6c7b14e6e8ef9a5ff6f
Jakubach <jakubach@gmail.com>
2023-11-14 15:33:49 +0100

    FIX setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
         (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name,'meshes'), glob('meshes/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
